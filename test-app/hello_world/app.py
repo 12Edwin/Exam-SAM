19a2326b751abbce50e116791bd7a423f10d0216
Edwin <20213tn046@utez.edu.mx>
2024-05-29 10:48:31 -0600

    [UPDATE] name label

--- test-app/hello_world/app.py
+++ test-app/hello_world/app.py
@@ -5,7 +5,7 @@ def lambda_handler():
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "name": "Edwin Yaret Barragan Valdovinos CR7",
+            "name": "Edwin Yaret Barragan Valdovinos CR7-FLUTTER",
             "grade": "9",
             "group": "C"
         }),
